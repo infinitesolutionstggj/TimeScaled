dbad2748d189594c60c9e7b75e734602ccafd3de
scorlibrian <william.carducci+github@gmail.com>
2014-04-25 18:04:33 -0400

    Added Camera Follow

---
+++ TimeScaledUnityProj/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	void Update()
+	{
+
+	}
+
+	void LateUpdate () 
+	{
+		AdjustCamera();
+	}
+
+	void AdjustCamera()
+	{
+		Vector3 playerPos = Player.Main.transform.position;
+		Vector3 moveVec = Vector3.zero;
+		Vector3 velocity = Vector3.zero;
+		float smoothTime = 0.25f;
+
+		moveVec = Vector3.SmoothDamp(Camera.main.transform.position, playerPos, ref velocity, smoothTime);
+		moveVec.z = Camera.main.transform.position.z;
+
+		if (Camera.main.WorldToViewportPoint(playerPos).x > GameSettings.MIN_SCREEN_BOUNDS && Camera.main.WorldToViewportPoint(playerPos).x < GameSettings.MAX_SCREEN_BOUNDS)
+		{
+			moveVec.x = Camera.main.transform.position.x;
+		}
+
+		if (Camera.main.WorldToViewportPoint(playerPos).y > GameSettings.MIN_SCREEN_BOUNDS && Camera.main.WorldToViewportPoint(playerPos).y < GameSettings.MAX_SCREEN_BOUNDS)
+		{
+			moveVec.y = Camera.main.transform.position.y;
+		}
+
+		if (moveVec != Camera.main.transform.position)
+		{
+			Camera.main.transform.position = moveVec;
+		}
+	}
+}
--- TimeScaledUnityProj/Assets/Scripts/GameSettings.cs
+++ TimeScaledUnityProj/Assets/Scripts/GameSettings.cs
@@ -6,6 +6,11 @@ public static class GameSettings
 	public const float MAX_TIME_SCALE = 10.0f;
 	public const float MIN_TIME_SCALE = -10.0f;
 	public static bool IsPaused { get { return Time.timeScale == 0; } }
+
+	public const float CAMERA_PAN_SPEED = 7.5f;
+	public const float MIN_SCREEN_BOUNDS = 0.25f;
+	public const float MAX_SCREEN_BOUNDS = 0.75f;
+
 	public static void Pause()
 	{
 		Time.timeScale = 0;
