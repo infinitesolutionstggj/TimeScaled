1d9744bb4ef3df9a273028cd78533b754c35caad
dave85 <dlundrigan@georgebrown.ca>
2014-04-27 06:46:53 -0400

    Menus & Fix
    Basic menus
    Collision fixed

---
+++ TimeScaledUnityProj/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+    //bool lastTankStanding = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        /*if (GameObject.FindObjectsOfType<Player>().Length < 2)
+            Application.LoadLevel("End");*/
+
+	    /*foreach (Player p in GameObject.FindObjectsOfType<Player>())
+        {
+
+        }*/
+	}
+
+    void FixedUpdate()
+    {
+        if (GameObject.FindObjectsOfType<Player>().Length <= -1)
+            Application.LoadLevel("End");
+    }
+}
---
+++ TimeScaledUnityProj/Assets/Scripts/Menus/MenuButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using XboxCtrlrInput;
+
+
+public class MenuButton : MonoBehaviour
+{
+    //public bool selected;
+    public bool exitButton;
+    //public string levelName;
+    public Object Level = null;
+    public XboxButton buttonPress;
+    public XboxButton altButtonPress;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (exitButton && (XCI.GetButtonDown(buttonPress) || XCI.GetButtonDown(altButtonPress)))
+            Application.Quit();
+        else if (XCI.GetButtonDown(buttonPress) || XCI.GetButtonDown(altButtonPress))
+            if (Level != null) Application.LoadLevel(Level.name);
+	}
+}
