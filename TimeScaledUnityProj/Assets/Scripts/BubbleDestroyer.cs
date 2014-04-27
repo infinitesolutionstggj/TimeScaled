897da8e89d2b6039b6a9d0f01232d238a0cee12b
AdamEaton <adam.eaton5@gmail.com>
2014-04-27 03:55:57 -0400

    Disruptor
    dissssss... RUP tor!

---
+++ TimeScaledUnityProj/Assets/Scripts/BubbleDestroyer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleDestroyer : Bullet
+{
+	public static BubbleDestroyer PrefabDestroyer;
+
+	public static new float Radius
+	{
+		get
+		{
+			CheckLoadPrefab();
+
+			return PrefabDestroyer.gameObject.GetComponent<CircleCollider2D>().radius;
+		}
+	}
+
+	public static BubbleDestroyer Spawn(Vector3 position, float angle, float speed)
+	{
+		CheckLoadPrefab();
+
+		BubbleDestroyer output = Instantiate(PrefabDestroyer, position, Quaternion.Euler(0, 0, angle)) as BubbleDestroyer;
+		output.speed = speed;
+		output.angle = angle;
+		output.lifeTime = 2;
+		return output;
+	}
+
+	public static new void CheckLoadPrefab()
+	{
+		if (PrefabDestroyer == null)
+			PrefabDestroyer = Resources.Load<BubbleDestroyer>("BubbleDestroyer");
+	}
+
+	protected override void NewFixedUpdate()
+	{
+		base.NewFixedUpdate();
+	}
+
+	public override void Detonate()
+	{
+		CheckLoadPrefab();
+
+		Debug.Log("Detonation");
+		foreach (var bubble in affectingTimeBubbles)
+			Destroy(bubble.gameObject);
+
+		Destroy(gameObject);
+	}
+}
---
+++ TimeScaledUnityProj/Assets/Scripts/Tanks/Disruptor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisruptorHS : TankSpecialHS
+{
+
+}
+
+public class Disruptor : TankSpecial<DisruptorHS>
+{
+	protected override void NewFixedUpdate()
+	{
+		base.NewFixedUpdate();
+	}
+
+	protected override void ExecuteSpecialX()
+	{
+	}
+	protected override void ExecuteSpecialY()
+	{
+		BubbleDestroyer.Spawn(transform.position + MathLib.FromPolar(Player.Radius + ReverseBubbleSpawner.Radius, Player.TurretAngle).ToVector3(),
+			Player.TurretAngle, Player.shotSpeed);
+	}
+	protected override void ExecuteSpecialB()
+	{
+	}
+
+	protected override DisruptorHS GetCurrentHistoryState()
+	{
+		TankSpecialHS input = _GetCurrentHistoryState();
+		DisruptorHS output = new DisruptorHS();
+		output.coolDownX = input.coolDownX;
+		output.coolDownY = input.coolDownY;
+		output.coolDownB = input.coolDownB;
+		return output;
+	}
+
+	protected override void ApplyHistoryState(DisruptorHS state)
+	{
+		_ApplyHistoryState(state);
+	}
+
+	private TimeBubbleLimits ClonePlayerTBL()
+	{
+		TimeBubbleLimits tbl = new TimeBubbleLimits();
+		tbl.sweetSpotAge = Player.timeBubbleLimits.sweetSpotAge;
+		tbl.sweetSpotSensitivity = Player.timeBubbleLimits.sweetSpotSensitivity;
+		tbl.minRadius = Player.timeBubbleLimits.minRadius;
+		tbl.maxRadius = Player.timeBubbleLimits.maxRadius;
+		tbl.minLifeSpan = Player.timeBubbleLimits.minLifeSpan;
+		tbl.maxLifeSpan = Player.timeBubbleLimits.maxLifeSpan;
+		tbl.minInnerRadiusPercentage = Player.timeBubbleLimits.minInnerRadiusPercentage;
+		tbl.maxInnerRadiusPercentage = Player.timeBubbleLimits.maxInnerRadiusPercentage;
+		tbl.minTimeScaleMultiplier = Player.timeBubbleLimits.minTimeScaleMultiplier;
+		tbl.maxTimeScaleMultiplier = Player.timeBubbleLimits.maxTimeScaleMultiplier;
+		return tbl;
+	}
+}
