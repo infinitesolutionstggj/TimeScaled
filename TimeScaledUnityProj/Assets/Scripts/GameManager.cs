495fef4395bb20e8e6d673c0dc131039d1629cb0
dave85 <dlundrigan@georgebrown.ca>
2014-04-27 05:21:16 -0400

    Menus

--- TimeScaledUnityProj/Assets/Scripts/GameManager.cs
+++ TimeScaledUnityProj/Assets/Scripts/GameManager.cs
@@ -19,7 +19,7 @@ public class GameManager : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		Instantiate(playerPrefab, new Vector3(0, 0, -2), Quaternion.identity);
 	}
 	
 	void Update()
