a87ecd278bf648d92044965365d8e134ec2b95d9
scorlibrian <william.carducci+github@gmail.com>
2014-04-27 11:15:09 -0400

    Added MovePosition Rigidbody extension

---
+++ TimeScaledUnityProj/Assets/Scripts/Rigidbody2DExt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Rigidbody2DExt
+{
+	private const float MAX_DISTANCE_DELTA = 0.25f;
+
+	public static void MovePosition(this Rigidbody2D rb, Vector2 displacement)
+	{
+		float steps = Mathf.Ceil(displacement.magnitude / MAX_DISTANCE_DELTA);
+		Vector2 stepDisplacement = displacement / steps;
+		for (int i = 0; i < steps; i++)
+		{
+			rb.transform.position += stepDisplacement.ToVector3();
+		}
+	}
+
+	public static void MovePositionTo(this Rigidbody2D rb, Vector2 targetPosition)
+	{
+		rb.MovePosition(targetPosition.ToVector3() - rb.transform.position);
+	}
+}
